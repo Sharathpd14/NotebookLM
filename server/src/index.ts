027fbcddd373e1f7969a941e8c366558d0d28151
Sharathpd14 <sharathpd14@gmail.com>
2026-08-20 12:40:29 +0530

    Better-auth integration is done and database schema is updated.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,17 @@
 import express from "express"
+import { toNodeHandler } from "better-auth/node"
+import {auth} from "./lib/auth.js"
 import "dotenv/config"
 
 const app = express()
 const PORT = process.env.PORT
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json())
+
 app.listen(8081, ()=>{
+    
     console.log(`Server is running on port no. 8081`)
 })
 
